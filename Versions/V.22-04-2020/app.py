7b2987497d523de112eed09fb933e117e89d4ffd
giannis <ggiannis@tudelft.nl>
2020-04-29 09:21:33 +0200

    added comments

--- Versions/V.22-04-2020/app.py
+++ Versions/V.22-04-2020/app.py
@@ -36,48 +36,56 @@ with app.app_context():
 def index3():
     return render_template('testinggraphs1.html')
 
+#graph 1
 @app.route('/difftasks')
 def difftasks():
     return jsonify({'difftasks': query_db(
         "SELECT task as lbl, count(*) as val from user_tasks group by task order by count(*) desc limit 10 "
     )})
 
+#graph 2
 @app.route('/sql')
 def results():
     return jsonify({'results': query_db(
         "SELECT count(succeeded) as val,course as lbl from user_tasks where succeeded='true' GROUP BY succeeded,course"
     )})
 
+#graph 2 
 @app.route('/sql1')
 def results1():
     return jsonify({'results1': query_db(
         "SELECT count(succeeded) as val,course as lbl from user_tasks where succeeded='false' GROUP BY succeeded,course"
     )})
 
+#graph 1 top 10 most tried exercises and how many succeed them
 @app.route('/percent')
 def percent():
     return jsonify({'percent': query_db(
         "select task, count(*) as val from user_tasks  where task in (" + request.args.get('tasks', '') + ") and succeeded='true' group by task order by 2 DESC;"
     )})
 
+#graph 3 avg number of tries for most tried exercises
 @app.route('/try')
 def avgtries():
     return jsonify({'avgtries': query_db(
         'select avg(tried) as val, task as lbl from user_tasks group by task order by avg(tried) desc limit 30'
     )})
 
+#submenu LSINF1101-PYTHON 
 @app.route('/topten1')
 def top10_LSINF1101():
     return jsonify({'topten1': query_db(
         'SELECT task as lbl from user_tasks where course="LSINF1101-PYTHON" group by task order by count(*) desc limit 10'
     )})
 
+#submenu LSINF1252
 @app.route('/topten2')
 def top10_LSINF1252():
     return jsonify({'topten2': query_db(
         'SELECT task as lbl from user_tasks where course="LSINF1252" group by task order by count(*) desc limit 10'
     )})
 
+#submenu LEPL1402
 @app.route('/topten3')
 def top10_LEPL1402():
     return jsonify({'topten3': query_db(
